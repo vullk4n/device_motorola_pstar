9183acdbae3abc033611ef6e72da43e82d0ce9c2
LuK1337 <priv.luk@gmail.com>
2022-03-25 20:58:27 +0100

    pstar: Replace AndroidBoard.mk modules copy with BOOT_KERNEL_MODULES
    Hacky copy rules were prone to fail because of some kind of race
    condition.
    
    Change-Id: I30d50a9dd87de9d57a583968f9936b84ac16cfca

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,6 +20,10 @@ DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 # Kernel
 TARGET_KERNEL_CONFIG := vendor/lineageos_pstar_defconfig
 
+# Kernel Modules
+BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load.recovery))
+BOOT_KERNEL_MODULES := $(BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD)
+
 # SEPolicy
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
