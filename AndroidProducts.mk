1c6cb1b15a05f9b7b92d7e63b149660aca8a07cf
vulkan-ops <vulkanops@gmail.com>
2024-04-27 17:45:18 -0300

    pstar: Initial derp bringup Signed-off-by: vullk4n <vulkanops@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_pstar.mk
+    $(LOCAL_DIR)/derp_pstar.mk
--- derp_pstar.mk
+++ derp_pstar.mk
@@ -8,23 +8,25 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common derp stuff.
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
 # Inherit from pstar device
 $(call inherit-product, device/motorola/pstar/device.mk)
 
+DERP_BUILDTYPE := Official
+TARGET_USES_MINE_GAPPS := true
+
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_pstar
+PRODUCT_NAME := derp_pstar
 PRODUCT_DEVICE := pstar
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := motorola edge 20 pro
 PRODUCT_MANUFACTURER := motorola
-
 PRODUCT_SYSTEM_NAME := pstar_retail
 
 # Build info
-#BUILD_FINGERPRINT := "motorola/pstar_retail/pstar:13/T1RAS33.55-15-16-5/d9def9-6b8134:user/release-keys"
+# BUILD_FINGERPRINT := "motorola/pstar_retail/pstar:13/T1RAS33.55-15-16-5/d9def9-6b8134:user/release-keys"
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="pstar_retail-user 13 T1RAS33.55-15-16-5 d9def9-6b8134 release-keys" \
     TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
