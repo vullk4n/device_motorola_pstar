6e16fbc0749812f5791c35afd6e34df8593210fb
vulkan-ops <vulkanops@gmail.com>
2024-05-28 15:20:59 -0300

    pstar: decommonize specific stuff
    Signed-off-by: vullk4n <vulkanops@gmail.com>

--- device.mk
+++ device.mk
@@ -12,6 +12,9 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
 # Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-derp
+
 PRODUCT_PACKAGES += \
     FrameworksResPstar \
     SystemUIResPstar \
@@ -20,6 +23,7 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     $(LOCAL_PATH)/configs/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
 
