ff7a6f184b4db520ca0ca358cee1b4fe2584efb0
vullk4n <vulkanops@gmail.com>
2024-06-15 23:43:43 -0300

    pstar: decommonize audio_effects.xml
    Signed-off-by: vullk4n <vulkanops@gmail.com>

--- device.mk
+++ device.mk
@@ -20,6 +20,7 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     $(LOCAL_PATH)/configs/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
 
